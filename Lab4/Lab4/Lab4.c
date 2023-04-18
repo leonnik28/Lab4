02fef2d2554b0da039d43d0072f90289dab38314
leonnik28 <112748954+leonnik28@users.noreply.github.com>
2023-04-18 21:04:49 +0300

    Update Lab4.c

--- Lab4/Lab4/Lab4.c
+++ Lab4/Lab4/Lab4.c
@@ -1,18 +1,27 @@
-﻿#include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include "Tree.h"
 #include "TreeAPI.h"
 #include <locale.h>
 #include <string.h>
 
-int main() {
-    FILE* fp = fopen("database.txt", "r");
+
+int main()
+{
+    errno_t err;
+    FILE* fp;
+    err = fopen_s(&fp, "database.txt", "r");
+    file_open(err);
     Node* loaded_tree = load_tree(fp);
     if (loaded_tree == NULL) loaded_tree = create_node("Egypt");
     traverse(loaded_tree);
-    fp = fopen("database.txt", "w");
-    save_tree(loaded_tree, fp);
     fclose(fp);
+    errno_t err_2;
+    FILE* fp_out;
+    err_2 = fopen_s(&fp_out, "database.txt", "w");
+    file_open(err_2);
+    save_tree(loaded_tree, fp_out);
+    fclose(fp_out);
     free_tree(loaded_tree);
     return 0;
 }
